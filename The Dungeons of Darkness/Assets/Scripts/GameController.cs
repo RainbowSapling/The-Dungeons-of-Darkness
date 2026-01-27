4833a45914a1343cabf60513821a372611bdabfb
RainbowSapling <mirbj463@student.liu.se>
2026-01-27 13:08:49 +0100

    Basic room navigation working

--- The Dungeons of Darkness/Assets/Scripts/GameController.cs
+++ The Dungeons of Darkness/Assets/Scripts/GameController.cs
@@ -1,21 +1,35 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
     [HideInInspector] public RoomNavigation roomNavigation;
 
-    public AudioSource audioSource = new AudioSource();
+    // Plays audio
+    public AudioSource audioSource;
+
+    //Menu buttons for making choices in the game
+    public Button button1;
+    public Button button2;
+
 
     void Awake()
     {
-        roomNavigation = GetComponent<RoomNavigation>(); 
+        // Assign the room navigation script
+        roomNavigation = GetComponent<RoomNavigation>();
+
+        // Add event listeners to buttons
+        button1.onClick.AddListener(ChooseOption1);
+        button2.onClick.AddListener(ChooseOption2);
     }
 
     void Start()
     {
+        // Plays the audio of the first room
         PlayRoomAudio();
     }
 
+    // Plays the audio for the current room
     public void PlayRoomAudio()
     {
         AudioClip clip = roomNavigation.currentRoom.clip;
@@ -23,9 +37,19 @@ public class GameController : MonoBehaviour
         audioSource.Play();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ChooseOption1()
     {
-        
+        Debug.Log("option 1");
+        Room nextRoom = roomNavigation.currentRoom.exits[0].room;
+        roomNavigation.ChangeRoom(nextRoom);
     }
+
+    public void ChooseOption2()
+    {
+        Debug.Log("option 2");
+        Room nextRoom = roomNavigation.currentRoom.exits[1].room;
+        roomNavigation.ChangeRoom(nextRoom);
+    }
+
+
 }
