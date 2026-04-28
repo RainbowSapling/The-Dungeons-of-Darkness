fe6765722622da3aa12b35a753b167df2ed64903
RainbowSapling <mirbj463@student.liu.se>
2026-04-28 15:48:10 +0200

    Switch music scrip

--- The Dungeons of Darkness/Assets/Scripts/Music.cs
+++ The Dungeons of Darkness/Assets/Scripts/Music.cs
@@ -4,7 +4,13 @@ public class Music : MonoBehaviour
 {
 
     public static Music instance;
-    
+
+    // All music clips
+    [SerializeField] AudioClip village1;
+    [SerializeField] AudioClip village2;
+    [SerializeField] AudioClip forrest1;
+    [SerializeField] AudioClip forrest2;
+
     void Awake()
     {
         if (instance != null)
@@ -18,5 +24,13 @@ public class Music : MonoBehaviour
         }
     }
 
+    void Update()
+    { 
+        if(CurrentScene.currentScene.scene == "VD1" || CurrentScene.currentScene.scene == "TD6") 
+        { 
+            this.gameObject.GetComponent<AudioSource>().clip = village1;
+            this.gameObject.GetComponent<AudioSource>().Play();
+        }
+    }
     
 }
