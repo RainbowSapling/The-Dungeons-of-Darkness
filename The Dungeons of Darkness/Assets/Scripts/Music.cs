f2941c2682c40ffd4ca00a2503ffa5c183bb575d
RainbowSapling <mirbj463@student.liu.se>
2026-04-30 14:12:34 +0200

    Added music and removed unnecessary files

--- The Dungeons of Darkness/Assets/Scripts/Music.cs
+++ The Dungeons of Darkness/Assets/Scripts/Music.cs
@@ -6,13 +6,21 @@ public class Music : MonoBehaviour
     public static Music instance;
 
     // All music clips
-    [SerializeField] AudioClip village1;
-    [SerializeField] AudioClip village2;
-    [SerializeField] AudioClip forrest1;
-    [SerializeField] AudioClip forrest2;
+    AudioClip village1;
+    AudioClip village2;
+    AudioClip forrest1;
+    AudioClip forrest2;
+    AudioClip river1;
+
 
     void Awake()
     {
+        village1 = Resources.Load<AudioClip>("Village1");
+        village2 = Resources.Load<AudioClip>("Village2");
+        forrest1 = Resources.Load<AudioClip>("Forrest1");
+        forrest2 = Resources.Load<AudioClip>("Forrest2");
+        river1 = Resources.Load<AudioClip>("River1");
+
         if (instance != null)
         {
             Destroy(gameObject);
@@ -26,9 +34,19 @@ public class Music : MonoBehaviour
 
     void Update()
     { 
+        // Update music
+        
+        // Village daytime
         if(CurrentScene.currentScene.scene == "VD1" || CurrentScene.currentScene.scene == "TD6") 
         { 
             this.gameObject.GetComponent<AudioSource>().clip = village1;
+            this.gameObject.GetComponent<AudioSource>().Play();
+        }
+
+        // River
+        if (CurrentScene.currentScene.scene == "R1")
+        {
+            this.gameObject.GetComponent<AudioSource>().clip = river1;
             this.gameObject.GetComponent<AudioSource>().Play();
         }
     }
